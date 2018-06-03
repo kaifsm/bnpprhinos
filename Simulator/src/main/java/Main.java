2f414ff637e920b3614b23d9899f96e328ed7bc0
Kaif Mavani <kaifsm3@gmail.com>
2018-06-03 15:55:22 +0800

    Add files via upload
    Added ImageIcon to main

--- Simulator/src/main/java/Main.java
+++ Simulator/src/main/java/Main.java
@@ -1,9 +1,12 @@
+import javax.swing.ImageIcon;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
 		MyFrame f = new MyFrame();
+		ImageIcon img = new ImageIcon("resources/rhino.png");
+		f.setIconImage(img.getImage());
 		f.setVisible(true);
 	}
 }
